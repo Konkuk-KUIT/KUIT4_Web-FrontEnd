f32985de2550bea6e858883ae539ec1135930f5e
hamxxn <hamin031121@gmail.com>
2024-10-01 01:04:35 +0900

    updateTodo 메서드 구현 addTodo 메서드에서 id를 날짜로 넘겨주고 있는데 서버에서 id재정의하고 있어 서버가 생성한 id 사용

---
+++ week3/hamxxn/todo/app.js
@@ -0,0 +1,96 @@
+const todoListEl = document.getElementById("todoList");
+const todoInputEl = document.getElementById("todoInput");
+
+const API_URL = "http://localhost:3000/todos";
+
+fetch(API_URL)
+  .then((response) => response.json())
+  .then((data) => renderTodo(data))
+  .catch((error) => console.log("error: " + error));
+
+const renderTodo = (newTodos) => {
+  todoListEl.innerHTML = "";
+  newTodos.forEach((todo) => {
+    const listEl = document.createElement("li");
+    listEl.textContent = todo.title;
+    listEl.id = `todo-${todo.id}`;
+
+    const deleteEl = document.createElement("span");
+    deleteEl.textContent = "🗑️";
+    deleteEl.onclick = () => deleteTodo(todo.id);
+
+    const udpateEl = document.createElement("span");
+    udpateEl.textContent = "✏️";
+    udpateEl.onclick = () => updateTodo(todo.id, todo.title);
+
+    listEl.append(deleteEl);
+    listEl.append(udpateEl);
+    todoListEl.append(listEl);
+  });
+};
+
+const addTodo = () => {
+  const title = todoInputEl.value;
+  if (!title) return;
+
+  const newTodo = {
+    title,
+    createdAt,
+    completed: false,
+  };
+
+  fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ...newTodo, completed: false }),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      todoInputEl.value = "";
+      return fetch(API_URL);
+    })
+    .then((response) => response.json())
+    .then((data) => renderTodo(data));
+};
+const deleteTodo = (todoId) => {
+  console.log(todoId);
+  fetch(API_URL + "/" + todoId, {
+    method: "DELETE",
+  })
+    .then(() => fetch(API_URL))
+    .then((response) => response.json())
+    .then((data) => renderTodo(data));
+};
+//연필 이모지 누르면 input으로 바뀌고 enter 누르면 바뀜
+const updateTodo = (todoId, originalTitle) => {
+  const todoItem = document.querySelector(`#todo-${todoId}`);
+
+  const updateInput = document.createElement("input");
+  updateInput.type = "text";
+  updateInput.value = originalTitle;
+  todoItem.innerHTML = "";
+
+  todoItem.append(updateInput);
+  updateInput.onkeydown = () => {
+    console.log("inner");
+    if (event.key == "Enter") {
+      fetch(API_URL + "/" + todoId, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: updateInput.value,
+        }),
+      })
+        .then(() => {
+          todoItem.innerHTML = "";
+          return fetch(API_URL);
+        })
+        .then((response) => response.json())
+        .then((data) => renderTodo(data));
+    }
+  };
+};
