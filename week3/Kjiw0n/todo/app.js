ad06e6098140996e0990b4d860b636ffea80415d
Kjiw0n <rwd0904@naver.com>
2024-10-04 21:33:28 +0900

    feat: todoList 수정 기능 추가

---
+++ week3/Kjiw0n/todo/app.js
@@ -0,0 +1,84 @@
+const todoListEl = document.getElementById("todoList");
+const todoInputEl = document.getElementById("todoInput");
+
+const API_URL = "http://localhost:8080/todos";
+
+fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => renderTodo(data));
+
+const updateTodo = (todoId) => {
+    const updateTitle = todoInputEl.value;
+
+    fetch(API_URL + "/" + todoId, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: updateTitle }),
+    })
+        .then((response) => response.json())
+        .then(() => fetch(API_URL))
+        .then((response) => response.json())
+        .then((data) => renderTodo(data))
+        .catch((error) => console.error("Error updating todo:", error));
+};
+
+const renderTodo = (newTodos) => {
+    todoListEl.innerHTML = "";
+    newTodos.forEach((todo) => {
+        const listEl = document.createElement("li");
+        listEl.textContent = todo.title;
+        listEl.id = `todo-${todo.id}`;
+
+        const deleteEl = document.createElement("span");
+        deleteEl.textContent = "🗑️";
+        deleteEl.onclick = () => deleteTodo(todo.id);
+
+        const udpateEl = document.createElement("span");
+        udpateEl.textContent = "✏️";
+        udpateEl.onclick = () => updateTodo(todo.id, todo.title);
+
+        listEl.append(deleteEl);
+        listEl.append(udpateEl);
+        todoListEl.append(listEl);
+    });
+};
+
+const addTodo = () => {
+    const title = todoInputEl.value;
+    const date = new Date();
+    const createdAt = date.toDateString();
+
+    if (!title) return;
+
+    const newTodo = {
+        id: date.getTime(),
+        title,
+        createdAt,
+    };
+
+    fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...newTodo, completed: false }),
+    })
+        .then((response) => response.json())
+        .then(() => {
+            todoInputEl.value = "";
+            return fetch(API_URL);
+        })
+        .then((response) => response.json())
+        .then((data) => renderTodo(data));
+};
+
+const deleteTodo = (todoId) => {
+    fetch(API_URL + "/" + todoId, {
+        method: "DELETE",
+    })
+        .then(() => fetch(API_URL))
+        .then((response) => response.json())
+        .then((data) => renderTodo(data));
+};
