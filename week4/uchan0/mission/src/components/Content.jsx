e973817e27eaa1e4664144eb20607c60fe56738d
holdn2 <yoocy01@gmail.com>
2024-10-07 17:04:14 +0900

    조건부 렌더링 적용

--- week4/uchan0/mission/src/components/Content.jsx
+++ week4/uchan0/mission/src/components/Content.jsx
@@ -6,42 +6,47 @@ const Content = () => {
   return (
     <div>
       <article className="post">
-        {marketModel.items.map((item, index) => (
-          <section key={index} className="post__container">
-            <div className="product__container">
-              <ImageCollection imgName={item.image} />
-              <div className="product__info">
-                <span className="product__name">{item.title}</span>
-                <div className="product__upload--info">
-                  <span className="location--clickable">{item.location}</span>
-                  <span className="product__info--normaltext">·</span>
-                  <span className="product__info--normaltext">
-                    {item.timeAgo}
-                  </span>
+        {marketModel.items.map(
+          (item, index) =>
+            item.isSold === true && (
+              <section key={index} className="post__container">
+                <div className="product__container">
+                  <ImageCollection imgName={item.image} />
+                  <div className="product__info">
+                    <span className="product__name">{item.title}</span>
+                    <div className="product__upload--info">
+                      <span className="location--clickable">
+                        {item.location}
+                      </span>
+                      <span className="product__info--normaltext">·</span>
+                      <span className="product__info--normaltext">
+                        {item.timeAgo}
+                      </span>
+                    </div>
+                    <span className="product__price">{item.price}</span>
+                  </div>
                 </div>
-                <span className="product__price">{item.price}</span>
-              </div>
-            </div>
-            <div className="interest__container">
-              {item.comments > 0 && (
-                <>
-                  <ImageCollection imgName={"smallChat.svg"} />
-                  <span className="product__info--normaltext">
-                    {item.comments}
-                  </span>
-                </>
-              )}
-              {item.likes > 0 && (
-                <>
-                  <ImageCollection imgName={"heart.svg"} />
-                  <span className="product__info--normaltext">
-                    {item.likes}
-                  </span>
-                </>
-              )}
-            </div>
-          </section>
-        ))}
+                <div className="interest__container">
+                  {item.comments > 0 && (
+                    <>
+                      <ImageCollection imgName={"smallChat.svg"} />
+                      <span className="product__info--normaltext">
+                        {item.comments}
+                      </span>
+                    </>
+                  )}
+                  {item.likes > 0 && (
+                    <>
+                      <ImageCollection imgName={"heart.svg"} />
+                      <span className="product__info--normaltext">
+                        {item.likes}
+                      </span>
+                    </>
+                  )}
+                </div>
+              </section>
+            )
+        )}
       </article>
     </div>
   );
