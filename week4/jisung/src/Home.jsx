276385440811f8fc5fcf7ee5c30826b5f75b348b
jstar_00 <wltjd6300@naver.com>
2024-10-07 17:50:52 +0900

    [Chore] 주석 수정

--- week4/jisung/src/Home.jsx
+++ week4/jisung/src/Home.jsx
@@ -9,12 +9,9 @@ import BottomNav from "./components/BottomNav";
 import marketModel from "./models/MarketModel"; // Default import로 `marketModel` 가져오기
 import navigationBar from "./models/NavigationBar";
 
-
-// `Home` 컴포넌트에서 `marketModel`을 prop으로 받지 않고 바로 사용
 const Home = () => {
   return (
     <div>
-      {/* `marketModel`에서 `location`과 `items`를 prop으로 전달 */}
       <Header location={marketModel.location} />
       <Content products={marketModel.items} />
       <BottomNav navBarLists={navigationBar.items} />
