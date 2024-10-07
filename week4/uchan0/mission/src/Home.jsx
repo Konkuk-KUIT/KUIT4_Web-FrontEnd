bbfcf63a478ac32cb330e9faa228a9801a840ae3
holdn2 <yoocy01@gmail.com>
2024-10-07 16:21:28 +0900

    step1 complete 및 map함수 이용해서 배열 렌더링

--- week4/uchan0/mission/src/Home.jsx
+++ week4/uchan0/mission/src/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ImageCollection from "./ImageCollection";
 import "./styles.css";
+import "./reset.css";
+import marketModel from "./models/MarketModel";
 
 const Header = () => {
   return (
@@ -28,206 +30,42 @@ const Content = () => {
   return (
     <div>
       <article className="post">
-        <section className="post__container">
-          <div className="product__container">
-            <ImageCollection imgName={"airpod.svg"} />
-            <div className="product__info">
-              <span className="product__name">에어팟 프로</span>
-              <div className="product__upload--info">
-                <span className="location--clickable">군자동</span>
-                <span className="product__info--normaltext">·</span>
-                <span className="product__info--normaltext">3일전</span>
+        {marketModel.items.map((item, index) => (
+          <section key={index} className="post__container">
+            <div className="product__container">
+              <ImageCollection imgName={item.image} />
+              <div className="product__info">
+                <span className="product__name">{item.title}</span>
+                <div className="product__upload--info">
+                  <span className="location--clickable">{item.location}</span>
+                  <span className="product__info--normaltext">·</span>
+                  <span className="product__info--normaltext">
+                    {item.timeAgo}
+                  </span>
+                </div>
+                <span className="product__price">{item.price}</span>
               </div>
-              <span className="product__price">220,000원</span>
             </div>
-          </div>
-          <div className="interest__container">
-            <ImageCollection imgName={"smallChat.svg"} />
-            <span className="product__info--normaltext">3</span>
-            <ImageCollection imgName={"heart.svg"} />
-            <span className="product__info--normaltext">11</span>
-          </div>
-        </section>
-        <section className="post__container">
-          <div className="product__container">
-            <ImageCollection imgName={"product2.svg"} />
-            <div className="product__info">
-              <span className="product__name">바이레도 블랑쉬 50ml</span>
-              <div className="product__upload--info">
-                <span className="location--clickable">광진구 구의제3동</span>
-                <span className="product__info--normaltext">·</span>
-                <span className="product__info--normaltext">26초 전</span>
-              </div>
-              <span className="product__price">4,000원</span>
+            <div className="interest__container">
+              {item.comments > 0 && (
+                <>
+                  <ImageCollection imgName={"smallChat.svg"} />
+                  <span className="product__info--normaltext">
+                    {item.comments}
+                  </span>
+                </>
+              )}
+              {item.likes > 0 && (
+                <>
+                  <ImageCollection imgName={"heart.svg"} />
+                  <span className="product__info--normaltext">
+                    {item.likes}
+                  </span>
+                </>
+              )}
             </div>
-          </div>
-          <div className="interest__container">
-            <ImageCollection imgName={"heart.svg"} />
-            <span className="product__info--normaltext">2</span>
-          </div>
-        </section>
-        <section className="post__container">
-          <div className="product__container">
-            <ImageCollection imgName={"sandwich.svg"} />
-            <div className="product__info">
-              <span className="product__name">샌드위치</span>
-              <div className="product__upload--info">
-                <span className="location--clickable">동대문구 휘경동</span>
-                <span className="product__info--normaltext">·</span>
-                <span className="product__info--normaltext">끌올 59초 전</span>
-              </div>
-              <span className="product__price">220,000원</span>
-            </div>
-          </div>
-        </section>
-        <section className="post__container">
-          <div className="product__container">
-            <ImageCollection imgName={"iphone13.svg"} />
-            <div className="product__info">
-              <span className="product__name">아이폰 13프로맥스</span>
-              <div className="product__upload--info">
-                <span className="location--clickable">군자동</span>
-                <span className="product__info--normaltext">·</span>
-                <span className="product__info--normaltext">1일전</span>
-              </div>
-              <span className="product__price">1,000,000원</span>
-            </div>
-          </div>
-        </section>
-        <section className="post__container">
-          <div className="product__container">
-            <ImageCollection imgName={"coffeemachine.svg"} />
-            <div className="product__info">
-              <span className="product__name">커피머신</span>
-              <div className="product__upload--info">
-                <span className="location--clickable">구리시 교문1동</span>
-                <span className="product__info--normaltext">·</span>
-                <span className="product__info--normaltext">1초 전</span>
-              </div>
-              <span className="product__price">100,000원</span>
-            </div>
-          </div>
-        </section>
-        <section className="post__container">
-          <div className="product__container">
-            <img
-              src="http://placehold.co/240"
-              alt="product image"
-              className="product__img"
-            />
-            <div className="product__info">
-              <span className="product__name">에어팟 프로</span>
-              <div className="product__upload--info">
-                <span className="location--clickable">군자동</span>
-                <span className="product__info--normaltext">·</span>
-                <span className="product__info--normaltext">3일전</span>
-              </div>
-              <span className="product__price">220,000원</span>
-            </div>
-          </div>
-          <div className="interest__container">
-            <ImageCollection imgName={"smallChat.svg"} />
-            <span className="product__info--normaltext">3</span>
-            <ImageCollection imgName={"heart.svg"} />
-            <span className="product__info--normaltext">11</span>
-          </div>
-        </section>
-        <section className="post__container">
-          <div className="product__container">
-            <img
-              src="http://placehold.co/240"
-              alt="product image"
-              className="product__img"
-            />
-            <div className="product__info">
-              <span className="product__name">에어팟 프로</span>
-              <div className="product__upload--info">
-                <span className="location--clickable">군자동</span>
-                <span className="product__info--normaltext">·</span>
-                <span className="product__info--normaltext">3일전</span>
-              </div>
-              <span className="product__price">220,000원</span>
-            </div>
-          </div>
-          <div className="interest__container">
-            <ImageCollection imgName={"smallChat.svg"} />
-            <span className="product__info--normaltext">3</span>
-            <ImageCollection imgName={"heart.svg"} />
-            <span className="product__info--normaltext">11</span>
-          </div>
-        </section>
-        <section className="post__container">
-          <div className="product__container">
-            <img
-              src="http://placehold.co/240"
-              alt="product image"
-              className="product__img"
-            />
-            <div className="product__info">
-              <span className="product__name">에어팟 프로</span>
-              <div className="product__upload--info">
-                <span className="location--clickable">군자동</span>
-                <span className="product__info--normaltext">·</span>
-                <span className="product__info--normaltext">3일전</span>
-              </div>
-              <span className="product__price">220,000원</span>
-            </div>
-          </div>
-          <div className="interest__container">
-            <ImageCollection imgName={"smallChat.svg"} />
-            <span className="product__info--normaltext">3</span>
-            <ImageCollection imgName={"heart.svg"} />
-            <span className="product__info--normaltext">11</span>
-          </div>
-        </section>
-        <section className="post__container">
-          <div className="product__container">
-            <img
-              src="http://placehold.co/240"
-              alt="product image"
-              className="product__img"
-            />
-            <div className="product__info">
-              <span className="product__name">에어팟 프로</span>
-              <div className="product__upload--info">
-                <span className="location--clickable">군자동</span>
-                <span className="product__info--normaltext">·</span>
-                <span className="product__info--normaltext">3일전</span>
-              </div>
-              <span className="product__price">220,000원</span>
-            </div>
-          </div>
-          <div className="interest__container">
-            <ImageCollection imgName={"smallChat.svg"} />
-            <span className="product__info--normaltext">3</span>
-            <ImageCollection imgName={"heart.svg"} />
-            <span className="product__info--normaltext">11</span>
-          </div>
-        </section>
-        <section className="post__container">
-          <div className="product__container">
-            <img
-              src="http://placehold.co/240"
-              alt="product image"
-              className="product__img"
-            />
-            <div className="product__info">
-              <span className="product__name">에어팟 프로</span>
-              <div className="product__upload--info">
-                <span className="location--clickable">군자동</span>
-                <span className="product__info--normaltext">·</span>
-                <span className="product__info--normaltext">3일전</span>
-              </div>
-              <span className="product__price">220,000원</span>
-            </div>
-          </div>
-          <div className="interest__container">
-            <ImageCollection imgName={"smallChat.svg"} />
-            <span className="product__info--normaltext">3</span>
-            <ImageCollection imgName={"heart.svg"} />
-            <span className="product__info--normaltext">11</span>
-          </div>
-        </section>
+          </section>
+        ))}
       </article>
     </div>
   );
