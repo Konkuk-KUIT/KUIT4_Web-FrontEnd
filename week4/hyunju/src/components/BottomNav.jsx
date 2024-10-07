6b2788bafe20fc5af9f570416840972096e79986
hyunju1211 <hyunju0411@konkuk.ac.kr>
2024-10-07 21:55:15 +0900

    commit message: step2 complete

---
+++ week4/hyunju/src/components/BottomNav.jsx
@@ -0,0 +1,36 @@
+const BottomNav = () => {
+    return (
+      <nav className="bottom-menu">
+          <div className="bottom-menus-btn">
+              <div className="bottom">
+                  <img className="menu-btn" src="assest/Home (2).svg" alt="홈 메뉴"/>
+                  <div className="btn-title">홈</div>  
+              </div>
+          </div>
+          <div className="bottom-menus-btn">
+              <div className="bottom">
+                  <img className="menu-btn" src="assest/News.svg" alt="동네생활 메뉴"/>
+                  <div className="btn-title">동네생활</div> 
+              </div> 
+          </div>
+          <div className="bottom-menus-btn">
+              <div className="bottom">
+                  <img className="menu-btn" src="assest/Place Marker.svg" alt="내 근처 메뉴"/>
+                  <div className="btn-title">내 근처</div>  
+              </div>  
+          </div>
+          <div className="bottom-menus-btn">
+              <div className="bottom">
+                  <img className="menu-btn" src="assest/Chat.svg" alt="채팅 메뉴"/>
+                  <div className="btn-title">채팅</div>  
+              </div>  
+          </div>
+          <div className="bottom-menus-btn">
+              <div className="bottom">
+                  <img className="menu-btn" src="assest/User.svg" alt="나의 당근 메뉴"/>
+                  <div className="btn-title">나의 당근</div>  
+              </div>  
+          </div>
+      </nav>
+    );
+  }
