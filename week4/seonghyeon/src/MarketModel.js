e6e25d3205d514e9127b035a36b46ee7e8d16f88
Hyeon <csh-scl>
2024-10-09 23:49:36 +0900

    feat : step1 complete

---
+++ week4/seonghyeon/src/MarketModel.js
@@ -0,0 +1,55 @@
+const marketModel = [
+  {
+    id: 1,
+    image: "./assets/product-airpot.svg",
+    title: "에어팟 프로",
+    location: "군자동",
+    date: "3일 전",
+    price: "220,000원",
+    chatCount: 3,
+    heartCount: 3,
+  },
+  {
+    id: 2,
+    image: "./assets/product2.svg",
+    title: "바이레도 블랑쉬 50ml",
+    location: "광진구 구의제3동",
+    date: "26초 전",
+    price: "4,000원",
+    chatCount: 3,
+    heartCount: 3,
+  },
+  {
+    id: 3,
+    image: "./assets/product3.svg",
+    title: "샌드위치",
+    location: "동대문구 휘경동",
+    date: "끌올 59초 전",
+    price: "8,000원",
+    chatCount: 0,
+    heartCount: 2,
+  },
+  {
+    id: 4,
+    image: "./assets/product4.svg",
+    title: "아이폰 프로맥스",
+    location: "군자동",
+    date: "1일 전",
+    price: "1,000,000원",
+    chatCount: 0,
+    heartCount: 0,
+  },
+  {
+    id: 5,
+    image: "./assets/product4.svg",
+    title: "커피머신",
+    location: "구리시 교문1동",
+    date: "1초 전",
+    price: "100,000원",
+    chatCount: 0,
+    heartCount: 0,
+  },
+];
+  
+  export default marketModel;
+  
