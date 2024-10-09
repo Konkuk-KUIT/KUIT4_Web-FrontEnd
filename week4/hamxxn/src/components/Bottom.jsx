44aa30a322cccfbc977ecfd6c20b29d8f9f7ce4c
hamxxn <hamin031121@gmail.com>
2024-10-09 11:42:55 +0900

    Bottom의 button 컴포넌트로 분리

--- week4/hamxxn/src/components/Bottom.jsx
+++ week4/hamxxn/src/components/Bottom.jsx
@@ -6,6 +6,17 @@ import ChatMessageTwo from "../assets/chat-message_two.svg";
 import User from "../assets/user.svg";
 
 import "../styles/BottomNav.css";
+const Button = (props) => {
+  const [src, text] = [props.src, props.text];
+  return (
+    <>
+      <button className="bottom-bar__item">
+        <img src={src} alt={text + "버튼"} className="bottom-bar__icon" />
+        <span className="bottom-bar__text">{text}</span>
+      </button>
+    </>
+  );
+};
 const BottomNav = () => {
   return (
     <>
@@ -13,38 +24,11 @@ const BottomNav = () => {
         <button className="plus__btn">
           <img alt="홈버튼" src={Plus} />
         </button>
-        <button className="bottom-bar__item">
-          <img src={Home_2} alt="홈 버튼" className="bottom-bar__icon" />
-          <span className="bottom-bar__text">홈</span>
-        </button>
-        <button className="bottom-bar__item">
-          <img
-            src={Ariticle}
-            alt="동네 생활 버튼"
-            className="bottom-bar__icon"
-          />
-          <span className="bottom-bar__text">동네생활</span>
-        </button>
-        <button className="bottom-bar__item">
-          <img
-            src={PlaceMarker}
-            alt="내 근처 버튼"
-            className="bottom-bar__icon"
-          />
-          <span className="bottom-bar__text">내 근처</span>
-        </button>
-        <button className="bottom-bar__item">
-          <img
-            src={ChatMessageTwo}
-            alt="채팅 버튼"
-            className="bottom-bar__icon"
-          />
-          <span className="bottom-bar__text">채팅</span>
-        </button>
-        <button className="bottom-bar__item">
-          <img src={User} alt="나의 당근 버튼" className="bottom-bar__icon" />
-          <span className="bottom-bar__text">나의 당근</span>
-        </button>
+        <Button src={Home_2} text={"홈"} />
+        <Button src={Ariticle} text="동네생활" />
+        <Button src={PlaceMarker} text="내 근처" />
+        <Button src={ChatMessageTwo} text="채팅 버튼" />
+        <Button src={User} text="나의 당근" />
       </nav>
     </>
   );
