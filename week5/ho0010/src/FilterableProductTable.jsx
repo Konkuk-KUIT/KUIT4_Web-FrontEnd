f5175a82b9ec91e6ea6d4a0faf0d3d4a83dd46fc
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-10-25 00:22:53 +0900

    feat: week5 setting

---
+++ week5/ho0010/src/FilterableProductTable.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
+import ProductTable from "./ProductTable";
+import InputBar from "./InputBar";
+
+const FilterableProductTable = ({ product, setProduct }) => {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  const addProduct = (newProduct) => {
+    setProduct((previousData) => [...previousData, newProduct]);
+  };
+
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
+      <ProductTable
+        product={product}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+      <InputBar addProduct={addProduct} />
+    </div>
+  );
+};
+
+export default FilterableProductTable;
