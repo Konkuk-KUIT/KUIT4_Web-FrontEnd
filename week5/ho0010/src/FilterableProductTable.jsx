ca17e7b917d2abd135c1053d8be0887deaa7096d
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-10-25 11:07:23 +0900

    refactor: product state
    FilterableProductTable에서 product조작에 관한 로직을 오로지 가지고 있는 것이 관심사가 적절

--- week5/ho0010/src/FilterableProductTable.jsx
+++ week5/ho0010/src/FilterableProductTable.jsx
@@ -3,7 +3,40 @@ import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
 import InputBar from "./InputBar";
 
-const FilterableProductTable = ({ product, setProduct }) => {
+const FilterableProductTable = () => {
+  const [product, setProduct] = useState([
+    { id: "1", category: "Fruits", price: 1, stocked: true, name: "Apple" },
+    {
+      id: "2",
+      category: "Fruits",
+      price: 1,
+      stocked: true,
+      name: "Dragonfruit",
+    },
+    {
+      id: "3",
+      category: "Fruits",
+      price: 2,
+      stocked: false,
+      name: "Passionfruit",
+    },
+    {
+      id: "4",
+      category: "Vegetables",
+      price: 2,
+      stocked: true,
+      name: "Spinach",
+    },
+    {
+      id: "5",
+      category: "Vegetables",
+      price: 4,
+      stocked: false,
+      name: "Pumpkin",
+    },
+    { id: "6", category: "Vegetables", price: 1, stocked: true, name: "Peas" },
+  ]);
+
   const [filterText, setFilterText] = useState("");
   const [inStockOnly, setInStockOnly] = useState(false);
 
