9687d33bd2e808eca61caa3767392471e05b43c7
holdn2 <yoocy01@gmail.com>
2024-10-28 19:09:00 +0900

    실습 완료

---
+++ week5/uchan0/src/InputBar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const InputBar = ({ addProduct }) => {
+  const [newProduct, setNewProduct] = useState({
+    id: "0",
+    category: "",
+    price: 0,
+    stocked: true,
+    name: "",
+  });
+
+  const handleChange = (value, label) => {
+    setNewProduct({ ...newProduct, [label]: value });
+  };
+
+  const handleAddNewProduct = () => {
+    const productWithId = { ...newProduct, id: Date.now().toString() };
+    addProduct(productWithId);
+    setNewProduct({ category: "", price: 0, stocked: true, name: "" });
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        value={newProduct.category}
+        onChange={(e) => handleChange(e.target.value, "category")}
+        placeholder="category..."
+      />
+      <input
+        type="text"
+        value={newProduct.price}
+        onChange={(e) => handleChange(e.target.value, "price")}
+        placeholder="price..."
+      />
+      <label>Is Stocked?</label>
+      <input
+        type="checkbox"
+        value={newProduct.stocked}
+        onChange={(e) => handleChange(e.target.checked, "stocked")}
+      />
+      <input
+        type="text"
+        value={newProduct.name}
+        onChange={(e) => handleChange(e.target.value, "name")}
+        placeholder="name..."
+      />
+      <button onClick={handleAddNewProduct} type={"button"}>
+        Add!
+      </button>
+    </form>
+  );
+};
+
+export default InputBar;
---
+++ week5/uchan0/src/SearchBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const SearchBar = ({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange,
+}) => {
+  const handleTextChange = (e) => {
+    onFilterTextChange(e.target.value);
+  };
+  const handleCheckboxChange = (e) => {
+    onInStockOnlyChange(e.target.checked);
+  };
+  return (
+    <form>
+      <input
+        type="text"
+        value={filterText}
+        placeholder="Search..."
+        onChange={handleTextChange}
+      />
+      <label>
+        <input
+          value={inStockOnly}
+          type="checkbox"
+          onChange={handleCheckboxChange}
+        />
+        Only show products in stock
+      </label>
+    </form>
+  );
+};
+
+export default SearchBar;
