26ad7be86c62777e8a9b7e0e9d9d8f41cda4520b
김지환 (Jihwan Kim) <kjhwan0802@naver.com>
2024-11-02 08:33:10 +0900

    feat: 6주차 mission code

---
+++ week6/turtlehwan/mission/src/components/Button.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+const sizeStyles = {
+  sm: {
+    padding: "8px 14px 8px 15px",
+    fontSize: "13px",
+  },
+  lg: {
+    padding: "10px 16px",
+    fontSize: "15px",
+  },
+  xl: {
+    padding: "18px 112px 19px 113px",
+    fontSize: "16px",
+  },
+};
+
+const StyledButton = styled.button`
+  color: white;
+  background-color: ${(props) => (props.disabled ? "#D0DFFB" : "#3182f6")};
+  font-weight: 500;
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+  border-width: 0;
+  border-radius: 8px;
+  padding: ${(props) => sizeStyles[props.size]?.padding};
+  font-size: ${(props) => sizeStyles[props.size]?.fontSize};
+`;
+
+const Button = ({
+  children,
+  type = "button",
+  size = "sm",
+  width = "auto",
+  height = "auto",
+  disabled,
+}) => {
+  return (
+    <StyledButton
+      type={type}
+      size={size}
+      width={width}
+      height={height}
+      disabled={disabled}
+    >
+      {children}
+    </StyledButton>
+  );
+};
+
+export default Button;
