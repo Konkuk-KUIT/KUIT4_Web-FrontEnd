ef2a6c09dedaf94aa1c1314ba61a12fd4858d98d
김지환 (Jihwan Kim) <kjhwan0802@naver.com>
2024-11-02 13:37:29 +0900

    feat: css, css-in-js

--- week6/turtlehwan/lecture/src/pages/Router.jsx
+++ week6/turtlehwan/lecture/src/pages/Router.jsx
@@ -1,21 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Page from "./Page/Page";
 import PageId from "./PageId/PageId";
-import Home from "./Home/Home";
+import Home from "./home/Home";
+
+const Layout = () => {
+  return (
+    <>
+      <header style={{ backgroundColor: "green" }}>헤더입니다</header>
+      <Outlet />
+    </>
+  );
+};
 
 const Router = () => {
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/page",
-      element: <Page />,
-    },
-    {
-      path: "/page/:pageId",
-      element: <PageId />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/page",
+          element: <Page />,
+        },
+        {
+          path: "/page/:pageId",
+          element: <PageId />,
+        },
+      ],
     },
   ]);
 
--- week6/turtlehwan/lecture/src/pages/home/Home.jsx
+++ week6/turtlehwan/lecture/src/pages/home/Home.jsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
+import s from "./Home.module.css";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  background-color: ${(props) => props.$bgcolor};
+`;
 
 const Home = () => {
   return (
     <div>
       Home
-      <h1>homehome</h1>
+      <h1 className={s.classclass}>homehome</h1>
+      <h1 className="classclass">homehome</h1>
       <Link to="/page">page로 가는 Link 입니다</Link>
+      <br />
       <Link to="/page/sdfsd">page/pageId 로 가는 Link 입니다</Link>
-      <p></p>
+      <br />
       <a href="/page">page</a>
+      <StyledDiv $bgcolor={"skyblue"}>설명설명설명</StyledDiv>
     </div>
   );
 };
