dccf8cc2456e9c3568211aa1875ffbbe2b9f839c
rlawngP91 <annie2104@naver.com>
2024-11-05 17:04:19 +0900

    feat : lecture code Heder Layout practice

--- week6/rlawngP91/vite-project/src/pages/Router.jsx
+++ week6/rlawngP91/vite-project/src/pages/Router.jsx
@@ -1,19 +1,33 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Home from "./home/Home";
 import Page from "./page/page";
 import PageId from "./page/pageId/PageId";
 
+const Layout = () => {
+    return (
+        <>
+            <header>This is a header</header>
+            <Outlet />
+        </>
+    );
+};
+
 const Router = () => {
     const router = createBrowserRouter([
         {
-            path: "/",
-            element: <Home />
-        }, {
-            path: "/page",
-            element: <Page />
-        }, {
-            path: "/page/:pageId",
-            element: <PageId />
+            element: <Layout />,
+            children: [
+                {
+                    path: "/",
+                    element: <Home />
+                }, {
+                    path: "/page",
+                    element: <Page />
+                }, {
+                    path: "/page/:pageId",
+                    element: <PageId />
+                }
+            ]
         }
     ]);
     return <RouterProvider router={router} />;
