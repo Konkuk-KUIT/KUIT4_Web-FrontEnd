281901c43c4c11b517c562a8e0c93ac7644fc5ec
rlawngP91 <annie2104@naver.com>
2024-11-05 16:53:55 +0900

    feat : lecture code Query string practice

--- week6/rlawngP91/vite-project/src/pages/page/pageId/PageId.jsx
+++ week6/rlawngP91/vite-project/src/pages/page/pageId/PageId.jsx
@@ -1,13 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation, useSearchParams } from "react-router-dom";
 
 const PageId = () => {
     const { pageId } = useParams();
+    const location = useLocation();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const onChangeParams = () => {
+        setSearchParams({ key: 1234 })
+    }
 
     return (
         <div>PageId
-            <div>
-                PageId : {pageId}
-            </div>
+            <div>PageId : {pageId}</div>
+            <div>PathName : {location.pathname}</div>
+            <div>QueryString : {location.search}</div>
+            <div>SearchParams : {searchParams.get("key")}</div>
+            <button onClick={onChangeParams}>
+                Change Key Value
+            </button>
         </div>
     )
 }
