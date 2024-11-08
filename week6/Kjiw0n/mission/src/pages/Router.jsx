623caf47a43240c0faac188d39542e24b9b55e61
Kjiw0n <rwd0904@naver.com>
2024-11-08 21:08:46 +0900

    feat: StatusBar 헤더 추가로 인한 라우터 수정

--- week6/Kjiw0n/mission/src/pages/Router.jsx
+++ week6/Kjiw0n/mission/src/pages/Router.jsx
@@ -1,26 +1,42 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Home from "./Home/Home";
 import Stores from "./Stores/Stores";
 import Store from "./Store/Store";
 import Cart from "./Cart/Cart";
 
+const Layout = () => {
+  return (
+    <>
+      <header>
+        <img src="StatusBar.svg" alt="statusBar image" />
+      </header>
+      <Outlet />
+    </>
+  );
+};
+
 const Router = () => {
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/store",
-      element: <Stores />,
-    },
-    {
-      path: "/store/:storeId",
-      element: <Store />,
-    },
-    {
-      path: "/cart",
-      element: <Cart />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/store",
+          element: <Stores />,
+        },
+        {
+          path: "/store/:storeId",
+          element: <Store />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+      ],
     },
   ]);
 
