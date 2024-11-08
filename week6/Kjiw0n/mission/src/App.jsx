e68b9157d282eaf3f2329fedf39e108e9c41dfd3
Kjiw0n <rwd0904@naver.com>
2024-11-08 21:02:41 +0900

    style: globalStyle 적용(width, height 지정)

--- week6/Kjiw0n/mission/src/App.jsx
+++ week6/Kjiw0n/mission/src/App.jsx
@@ -1,10 +1,12 @@
 import Router from "./pages/Router";
 import { Normalize } from "styled-normalize";
+import GlobalStyle from "./styles/GlobalStyle";
 
 const App = () => {
   return (
     <>
       <Normalize />
+      <GlobalStyle />
       <Router />
     </>
   );
