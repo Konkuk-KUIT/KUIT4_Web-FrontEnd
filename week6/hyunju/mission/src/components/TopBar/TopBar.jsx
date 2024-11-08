b49c7a4a746b939aed4d700fe211514b41ff34b7
hyunju1211 <hyunju0411@konkuk.ac.kr>
2024-11-08 17:41:12 +0900

    wheyerror....

---
+++ week6/hyunju/mission/src/components/TopBar/TopBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import leftArrow from './assets/leftArrow.svg'
+const TopBar = () => {
+  return (
+    <div>
+      <img className=''src={leftArrow} />
+    </div>
+  )
+}
+
+export default TopBar
---
+++ week6/hyunju/mission/src/pages/Home/Home.jsx
@@ -0,0 +1,32 @@
+// import Button from 'src/components/Button/Button.jsx';
+// import styles from './Home.module.css';
+import OrderBar from '../../components/OrderBar/OrderBar'
+
+
+const Home = () => {
+  const food = [
+    { name: '피자', icon: '🍕' },
+    { name: '샐러드', icon: '🥗' },
+  
+  ];
+  
+  return( 
+    <>
+        <div className={styles['food-menu-container']}>
+            <h2 className={styles['title']}>오늘은 무엇을 먹을까요?</h2>
+            <p className={styles['sub-title']}>한남중앙로 40길 (한남 빌리지)로 배달 &gt;</p>
+            <div className={styles['grid-container']}>
+                {foodOptions.map((item, index) => (
+                    <div className={styles['grid-item']} key={index}>
+                        <div className={styles['icon']} role="img" aria-label={item.name}>{item.icon}</div>
+                        <span className={styles['item-name']}>{item.name}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+        <OrderBar />
+        </>
+  );
+};
+
+export default Home;
