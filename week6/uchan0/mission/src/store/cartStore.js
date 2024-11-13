f6f198f46da2041b7f10ee483a5e46971612a2ca
holdn2 <yoocy01@gmail.com>
2024-11-13 18:30:42 +0900

    동일 품목 개수 반영 완료(stores.js에 cnt 추가). 주문취소 시 0으로 초기화 미션완료

--- week6/uchan0/mission/src/store/cartStore.js
+++ week6/uchan0/mission/src/store/cartStore.js
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import allStore from "../models/stores";
 
 const initialState = {
   store: undefined,
@@ -16,9 +17,12 @@ const useCartStore = create((set) => ({
     set((state) => ({ ...state, store: store }));
   },
   resetAll: () => {
-    set((state) => ({
-      menus: state.menus.map((menu) => ({ ...menu, cnt: 0 })),
-    }));
+    // allStores의 모든 메뉴의 cnt를 0으로 초기화
+    allStore.forEach((store) => {
+      store.menus.forEach((menu) => {
+        menu.cnt = 0;
+      });
+    });
     set(initialState);
   },
   updateMenuCount: (menuId) => {
