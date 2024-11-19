f1887111a554a666f0a1f5b94b412265ced20740
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2024-11-19 16:41:11 +0900

    week7 mission correction

--- week6/heeedragon/mission/src/pages/Cart/Cart.jsx
+++ week6/heeedragon/mission/src/pages/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../components/Header";
 import useCartStore from "./cartStore";
 import styles from "./Cart.module.css";
+import Button from "../../components/Button";
 
 const Cart = () => {
     const menus = useCartStore((state) => state.menus);
@@ -13,62 +14,78 @@ const Cart = () => {
     const minimumOrderAmount = store?.minDeliveryPrice || 0;
     const deliveryFee = store?.deliveryFee || 0;
 
+    const handlePayment = () => { };
+
     return (
         <div>
             <Header />
             <div>
-                <div>
-                    <div>배달주소</div>
-                    <div>주소가 있어요</div>
-                    <img src=""></img>
-                </div>
+
+                <div className={styles.gap}></div>
 
                 <div>
-                    <div>{store?.name}</div>
-                    <div>
-                        {totalOrderAmount > minimumOrderAmount ? (<></>) :
-                            (
-                                <div>
-                                    <div>최소금액 미달</div>
-                                    <img src="" alt="주문 불가" />
-                                </div>
-                            )}
-                    </div>
-                    <>
-
-                        <div>
-                            <div>
-                            {menus.map((menu) => (
-                                <div key={menu.id} className={styles.menuRow}>
+                    <div className={styles.storeTitle}>
+                        <div className={styles.storeName}>{store?.name}</div>
+                        <div className={styles.orderDeny}>
+                            {totalOrderAmount > minimumOrderAmount ? (<></>) :
+                                (
                                     <div>
-                                        <h3>{menu.name}</h3>
-                                        <p>{menu.ingredients}</p>
-                                        <p>{menu.price}원</p>
+                                        <div>최소금액 미달</div>
+                                        <img src="" />
                                     </div>
-                                    <div>
-                                        <div>{menu.quantity}개</div>
+                                )}
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            {menus.map((menu) => (
+                                <div key={menu.id} className={styles.menuDetail}>
+                                    <div className={styles.menuDec}>
+                                        <div className={styles.menuName}>{menu.name}</div>
+                                        <div className={styles.menuIng}>{menu.ingredients}</div>
+                                        <div className={styles.menuPrice}>{menu.price}원</div>
+                                    </div>
+                                    <div className={styles.menuCheck}>
+                                        <div className={styles.menuQuan}>{menu.quantity}개</div>
+                                        <img src="" alt=">"></img>
                                     </div>
                                 </div>
                             ))}
-                            </div>
+                        </div>
 
-                            <div className={styles.summary}>
-                                <div>주문금액</div><div>{totalOrderAmount}원</div>
-                                <div>배달요금</div><div>{deliveryFee}원</div>
-                                <div>
-                                    총 결제금액</div><div>{(totalOrderAmount + deliveryFee)}원
-                                </div>
+                        <div className={styles.returnStore}>
+                            <div>더 담기</div>
+                            <img src="../../../public/plusIcon.svg"></img>
+                        </div>
+
+                        <div className={styles.gap}></div>
+
+                        <div className={styles.summary}>
+                            <img></img>
+                            <div className={styles.summaryRow}>
+                                <div className={styles.text}>주문금액</div>
+                                <div className={styles.amount}>{totalOrderAmount}원</div>
                             </div>
-                            <div>
-                                최소 주문금액 {minimumOrderAmount}원
+                            <div className={styles.summaryRow}>
+                                <div className={styles.text}>배달요금</div>
+                                <div className={styles.amount}>{deliveryFee}원</div>
+                            </div>
+                            <div className={styles.summaryRow}>
+                                <div className={styles.text2}>총 결제금액</div>
+                                <div className={styles.amount2}>{(totalOrderAmount + deliveryFee)}원</div>
                             </div>
                         </div>
-                    </>
+
+                        <div className={styles.minimumOrderAmount}>
+                            최소 주문금액 {minimumOrderAmount}원
+                        </div>
+                    </div>
                 </div>
 
-                <button disabled={totalOrderAmount < minimumOrderAmount}>
+                <Button onClick={handlePayment} type="button" size="xl" disabled={totalOrderAmount < minimumOrderAmount}>
                     {totalOrderAmount}원 결제하기
-                </button>
+                </Button>
             </div>
         </div>
     );
@@ -76,4 +93,10 @@ const Cart = () => {
 
 export default Cart;
 
-
+/*
+<div>
+<div>배달주소</div>
+<div>주소가 있어요</div>
+<img src=""></img>
+</div>
+*/
