308c906270cde7b9a9546b90fe20b0c939b692c5
jstar_00 <wltjd6300@naver.com>
2024-11-21 20:08:43 +0900

    [Fix] 키패드 숫자가 클릭 시마다 변경되는 문제 수정
    - keypadNumbers의 shuffle() 메서드를 useMemo로 감싸 리렌더링 시에도 동일한 숫자 배열을 유지하도록 수정

--- week8/jisung/src/components/PasswordInput/index.jsx
+++ week8/jisung/src/components/PasswordInput/index.jsx
@@ -4,6 +4,7 @@ import NumberKeypads from '../NumberKeypads';
 import { useEffect, useState } from 'react';
 import { DEFAULT_PASSWORD_LENGTH } from '../../constants/password';
 import shuffle from '../../utils/shuffle';
+import { useMemo } from 'react';
 
 export default function PasswordInput({
   title,
@@ -20,13 +21,17 @@ export default function PasswordInput({
     }
   }, [isPasswordCompleted]);
 
-  const keypadNumbers = shuffle(
-    Array.from({ length: 10 }, (_, index) => index),
+  const keypadNumbers = useMemo(() => shuffle(
+    Array.from({ length: 10 }, (_, index) => index)),
+  [] // Dependency 배열을 비워놓으면 리렌더링 되더라도 항상 그대로 유지됨
   );
 
   const handleKeyPress = (number) => {
     setPassword((prev) => prev + number);
   };
+  // 이 메서드에 의해 비밀번호를 누를 때마다 password의 상태가 변경돼서 PasswordInput 컴포넌트가 리렌더링됨
+  // 따라서 keypadNumbers도 비밀번호를 누를 때마다 shuffle되고 리렌더링됨
+  // 이걸 방지하려면 useMemo로 keypadNumbers의 shuffle을 감싸야함
 
   return (
     <>
