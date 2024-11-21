5d137e1cfb13790e19611e907601b08b1c4e0e1b
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2024-11-21 23:45:10 +0900

    week8 fetch complete

--- week6/heeedragon/mission/src/App.jsx
+++ week6/heeedragon/mission/src/App.jsx
@@ -1,14 +1,7 @@
-import { useEffect } from "react";
 import Router from "./pages/Router";
 import { Normalize } from "styled-normalize";
-import {getStore} from "./apis/stores";
 
 const App = () => {
-
-  useEffect(() => {
-    getStore(1).then((value) => console.log(value));
-  }, [])
-
   return (
     <>
       <Normalize />
