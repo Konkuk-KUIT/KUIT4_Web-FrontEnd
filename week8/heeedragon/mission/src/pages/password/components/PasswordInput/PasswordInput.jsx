d974d9bfeda0363a0b26871bb883b01b98c8e63e
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2024-11-22 23:48:23 +0900

    week8 비밀번호 실습 완료

---
+++ week8/heeedragon/mission/src/pages/password/components/PasswordInput/PasswordInput.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+import Header from '../Header/Header';
+import PasswordIndicator from '../PasswordIndicator/PasswordIndicator';
+import NumberKeypads from '../NumberKeypads/NumberKeypads';
+import shuffle from '../../utils/shuffle';
+import { useEffect, useMemo } from 'react';
+
+export function PasswordInput({ title, subtitle, length, onComplete }) {
+    const [password, setPassword] = useState('');
+
+    const isComplete = password.length === length;
+
+    const shuffledNumbers = useMemo(
+        () => shuffle(Array.from({ length: 10 }).map((_, index) => index)),
+        [],
+    );
+
+    const handleKeypadPress = (value) => {
+        setPassword((prev) => prev + value);
+    };
+
+    const resetPassword = () => {
+        setPassword('');
+    };
+
+    useEffect(() => {
+        if (isComplete) {
+            onComplete(password);
+            resetPassword();
+        }
+    }, [isComplete]);
+
+    return (
+        <>
+            <Header title={title} subtitle={subtitle} />
+            <PasswordIndicator length={length} value={password} />
+            <NumberKeypads
+                numbers={shuffledNumbers}
+                onPress={handleKeypadPress}
+                secure={true}
+            />
+        </>
+    );
+}
+
+export default PasswordInput;
